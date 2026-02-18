c149b9e4c4a47b41bffba6a30d6e13406e603033
starscreemm <starscreemm8@gmail.com>
2026-02-18 16:09:11 +0200

    add docker vars

---
+++ src/main/java/org/example/config/TestH2Config.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("org.example")
+public class TestH2Config {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;MODE=PostgreSQL");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
+}
