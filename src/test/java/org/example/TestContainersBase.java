76e1aa0a63d31ca93be77575f3c8d71e1a50d663
starscreemm <starscreemm8@gmail.com>
2026-02-18 15:32:13 +0200

    add docker vars

--- src/test/java/org/example/TestContainersBase.java
+++ src/test/java/org/example/TestContainersBase.java
@@ -3,18 +3,18 @@ package org.example;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 
+@Testcontainers
 public abstract class TestContainersBase {
 
+    @Container
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
-
-    static {
-        postgreSQLContainer.start();
-    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
